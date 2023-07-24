1f63020a94316c58dbc1218c9589b09277681cb5
onigiri-w2 <94182619+onigiri-w2@users.noreply.github.com>
2023-07-24 17:45:08 +0900

    [mod] footerの背景色を無くした

--- src/components/elements/Footer/index.tsx
+++ src/components/elements/Footer/index.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <div className="flex justify-center bg-primary-800 py-4">
-      <span className="font-light text-gray-100">©️onigiri.w2 2023</span>
+    <div className="flex justify-center py-8">
+      <span className="font-light text-base-color-very-light">
+        ©️onigiri.w2 2023
+      </span>
     </div>
   );
 };
